9a06dd67689f9d8790d052563a28715deaef8a64
Bestwick <lbestwic@8c85900d6550.ant.amazon.com>
2018-03-25 15:10:27 -0700

    set port

---
+++ application.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/loganbestwick/simple-app/pkg/api"
+)
+
+func main() {
+	fmt.Println("Starting App...")
+	api.Setup()
+}
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func Setup() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/echo", echo)
-	log.Fatal(http.ListenAndServe(":8081", router))
+	router.HandleFunc("/health", health)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("value")
 	fmt.Fprintf(w, query)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
 }

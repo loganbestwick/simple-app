28f01f5b6fe929c3dda4d106f204301f4935eebf
Bestwick <lbestwic@8c85900d6550.ant.amazon.com>
2018-03-25 20:35:02 -0700

    deploy to beanstalk working

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-go get ./...
-go build -o bin/application ./cmd/main.go
+# Move the application into the correct package for $GOPATH
+DEST=$GOPATH/src/github.com/loganbestwick/simple-app
+rm -rf $DEST
+mkdir -p $DEST
+cp -r * $DEST
+
+# Build the server
+go build -o application $DEST/cmd/main.go
